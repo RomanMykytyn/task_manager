06610b2b651892ea4401cd6a00d0a4847a71cab8
Roman Mykytyn <roman.mykytyn89@gmail.com>
2019-05-19 19:10:51 +0300

    qwerty

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var User = require('./models/user');
+
+var passport = require('passport');
+var expressSession = require('express-session');
 
 var app = express();
 
+mongoose.connect('mongodb://localhost/task_manager', function (err) {
+   if (err) throw err;
+   console.log('Successfully connected');
+});
+
+
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +29,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 
 module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('User',{
+    username: {
+      type: String,
+      unique: true,
+      required: true
+  },
+    password: {
+      type: String,
+      required: true
+  },
+});
